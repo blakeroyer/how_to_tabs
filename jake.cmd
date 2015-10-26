4e2e3a703f0df8d906eb80c184c1c34864df6457
Blake Royer <blake@emmit.ca>
2015-10-26 13:16:28 -0400

    Fixed build script on Windows

--- jake.cmd
+++ jake.cmd
@@ -1,4 +1,10 @@
 
 @echo off
 
-node_modules/.bin/jake %*
+if exist node_modules\.bin\jake goto :run_jake
+
+echo Building npm modules:
+call npm rebuild
+
+:run_jake
+call node_modules\.bin\jake %*
