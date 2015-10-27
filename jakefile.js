0e5942287d0560dcffe66fc6104e8134d445e261
Blake Royer <blake@emmit.ca>
2015-10-26 20:15:55 -0400

    Installed JSHint

--- jakefile.js
+++ jakefile.js
@@ -1,12 +1,26 @@
+
 // Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
 (function() {
 	"use strict";
 
+	var semver = require("semver");
+
 	desc("Default build");
-	task("default", function() {
+	task("default", [ "version" ], function() {
 		console.log("\n\nBUILD OK");
 	});
 
+	desc("Check Node version");
+	task("version", function() {
+		console.log("Checking Node version: .");
+
+		var packageJson = require("./package.json");
+		var expectedVersion = packageJson.engines.node;
+
+		var actualVersion = process.version;
+		if (semver.neq(expectedVersion, actualVersion)) {
+			fail("Incorrect Node version: expected " + expectedVersion + ", but was " + actualVersion);
+		}
+	});
+
 }());
-
-
