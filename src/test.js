e6dd166e255ee1e4c6ec889bc1a02d3a6d632dd7
Blake Royer <blake@emmit.ca>
2015-10-28 16:22:05 -0400

    Added global Mocha variables to lint to avoid errors

--- src/test.js
+++ src/test.js
@@ -1,26 +1,26 @@
 // Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
-// Tried out git quick
-
 (function() {
     "use strict";
 
-var assert = require("chai").assert;
+    describe("Addition", function() {
 
-describe("Addition", function(){
+        it("adds positive numbers", function() {
+            assertEqual(add(3, 4), 7);
+        });
 
-    it("adds positive numbers", function() {
-        assert.equal(add(3, 4), 7,'Expected this but got that.');
+        it("uses IEEE 754 floating point", function() {
+            assertEqual(add(0.1, 0.2), 0.30000000000000004);
+        });
+
+        function assertEqual(actual, expected) {
+            if (actual !== expected) throw new Error("expected " + expected + ", but was " + actual);
+        }
+
     });
 
-    it("uses IEEE 754 floating point", function(){
-        assert.equal(add(0.1, 0.2), 0.30000000000000004);
-    });
 
-});
-
-
-function add(a, b) {
-    return a + b;
-}
+    function add(a, b) {
+        return a + b;
+    }
 
 }());
