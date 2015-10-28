7f75d47f61762a6114fe30e7733fb12570208ff0
Blake Royer <blake@emmit.ca>
2015-10-27 21:03:58 -0400

    Set up first manual test in tests.js

--- src/tests.js
+++ src/tests.js
@@ -1,7 +1,15 @@
 // Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
-
-
 (function() {
     "use strict";
+
+assertEqual(add(3, 4), 7);
+
+function add(a, b) {
+    return a - b;
+}
+
+function assertEqual(actual, expected) {
+    if actual !== expected) throw new Error("Expected " + expected + ", but got " + actual)
+}
 
 }());
