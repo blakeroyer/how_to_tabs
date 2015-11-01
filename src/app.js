5e158455e2dac98f51d656b7c6a9f4d865968bdb
Blake Royer <blake@emmit.ca>
2015-11-01 09:23:23 -0500

    Manually removed binaries that were accidentally checked in for karma-common-js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 // Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
 (function() {
-    "use strict";
+	"use strict";
 
-     exports.add = function add(a, b) {
-        return a + b;
-    };
+	
+
+	console.log("Hello!");
+
 
 }());
